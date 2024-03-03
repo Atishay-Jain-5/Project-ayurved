ae33b451d2d4a62985e3c92f3f5b31f9eb23509c
atishay jkain <atishay.jain@ssipmt.com>
2024-03-03 14:34:58 +0530

    nav bar changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,168 @@
-import React from 'react'
-//import tailwindConfig from '../../../tailwind.config'
-function Navbar() {
-  return (
-    <>
-    <div class="border-2 border-sky-500 font-extrabold">
-      asdkjhbvda
-    </div>
-    </>
-  )
-}
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
+import "../../App.css"
 
-export default Navbar
+import '@fontsource-variable/overpass';
+const pages = ['Home', 'About Us', 'Treatment', 'Knowledge'];
+//const settings = ['Profile', 'Account', 'Dashboard', 'Ayurvedut'];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+ // const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  // const handleOpenUserMenu = (event) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
+
+  return (
+    <div className='bg-white'>
+
+    <AppBar position="static" sx={{backgroundColor:"white",color:"black"}} >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <EmojiNatureIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} fontSize='large' />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'Overpass Variable',
+              fontWeight: 900,
+              letterSpacing: '.1rem',
+              color: 'black',
+              textDecoration: 'none',
+            }}
+            >
+            Ayurved
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="black"
+              >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+              >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center" color={'black'} sx={{ fontSize: '1.3rem' }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <EmojiNatureIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+             fontFamily: 'Overpass Variable',
+
+              fontWeight: 900,
+              letterSpacing: '.3rem',
+              color: 'black',
+              textDecoration: 'none',
+            }}
+            >
+            Ayurved
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex',alignItems:"center",justifyContent:"center",gap:"40px" }, }}>
+            {pages.map((page) => (
+              <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'black', display: 'block',fontSize: '1.1rem',fontFamily:"Overpass Variable",fontWeight:"bold"}}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          {/* <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+              >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box> */}
+        </Toolbar>
+      </Container>
+    </AppBar>
+              </div>
+  );
+}
+export default Navbar;
