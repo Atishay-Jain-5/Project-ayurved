df2767f019a4fcd22ededd9892eaca75a9e10f88
atishay jkain <atishay.jain@ssipmt.com>
2024-03-17 13:49:59 +0530

    2 more models added

--- ML-Files/models server/model.py
+++ ML-Files/models server/model.py
@@ -3,14 +3,18 @@ from joblib import load
 import numpy as np
 
 app = Flask(__name__)
-model_pathRF = r'D:\Atishay\homework\Project minor\RandomForest.joblib'
-model_pathk = r'D:\Atishay\homework\Project minor\classifierK.joblib'
-model_pathDT = r'D:\Atishay\homework\Project minor\DecisionTree.joblib'
-model_pathMLP = r'D:\Atishay\homework\Project minor\classifierMLP.joblib'
+model_pathRF = r'D:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\RandomForest.joblib'
+model_pathk = r'D:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\classifierK.joblib'
+model_pathDT = r'D:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\DecisionTree.joblib'
+model_pathMLP = r'D:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\classifierMLP.joblib'
+model_pathSVM = r'D:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\classifierSVM.joblib'
+model_pathNB = r'D:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\classifierNB.joblib'
 modelRF = load(model_pathRF)
 modelK = load(model_pathk)
 modelDT = load(model_pathDT)
 modelMLP = load(model_pathMLP)
+modelSVM = load(model_pathSVM)
+modelNB = load(model_pathNB)
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
@@ -20,7 +24,9 @@ def predict():
     predictionK = modelK.predict(input_data).tolist()
     predictionMLP = modelMLP.predict(input_data).tolist()
     predictionDT = modelDT.predict(input_data).tolist()
-    l1 = {"RF": predictionRF, "K": predictionK, "DT": predictionDT, "predictionMLP": predictionMLP}
+    predictionSVM = modelSVM.predict(input_data).tolist()
+    predictionNB = modelNB.predict(input_data).tolist()
+    l1 = {"RF": predictionRF, "K": predictionK, "DT": predictionDT, "predictionMLP": predictionMLP,"predictionSVM": predictionSVM,"predictionNB": predictionNB}
 
     return jsonify(l1)
 
