b8c15a8e81b4fc88b6631e92d20a86ebcfc85d6c
atishay jkain <atishay.jain@ssipmt.com>
2024-04-24 16:11:56 +0530

    minor changes

--- src/components/section3(text area)/Boxcomponent.js
+++ src/components/section3(text area)/Boxcomponent.js
@@ -3,37 +3,42 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import SymptomsContext from "../../context/SymptopsContext";
 import { Typography } from "@mui/material";
+import section3img from "./ig.png"
+
 export default function Boxcomponent() {
   const { Symptoms } = useContext(SymptomsContext);
   return (
+    <div className="flex flex-row justify-between">
     <Box
       sx={{
-       
-        display: "flex", alignItems: "center", justifyContent: "center",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      
         flexWrap: 'wrap',
-        marginTop:"50px",
-       
+        marginTop: "50px",
+        marginLeft: "20px",
         '& > :not(style)': {
           m: 1,
-          width: 1280,
-          height: 350,
+          width: 660,
+          height: 440,
         },
       }}
     >
-      
-      <Paper elevation={4} sx={{ backgroundColor:"whitesmoke", display: "flex", alignItems: "flex-start", justifyContent: "flex-start",flexDirection:"column",  }}>
-      {Symptoms.map((symptom, index) => (
-        <div>
-          <Paper elevation={3} sx={{borderRadius:"20px",marginLeft:"8px",marginTop:"15px"}}>
-          <Typography variant="h4" gutterBottom key={index} style={{fontFamily: "Courier New",padding:"5px"}}>
-      {symptom}
-
-      </Typography>
+      <Paper elevation={4} sx={{ backgroundColor: "#d1f1de", display: "flex", alignItems: "flex-start", justifyContent: "flex-start", flexDirection: "column" }}>
+        {Symptoms.map((symptom, index) => (
+          
+            <Paper elevation={1} sx={{ borderRadius: "10px", marginLeft: "8px", marginTop: "15px" }}>
+              <Typography variant="h5" gutterBottom style={{ fontFamily: "Courier New", padding: "3px" }}>
+                {symptom}
+              </Typography>
+            </Paper>
+        
+        ))}
       </Paper>
-        </div>
-     
-      ))}
-        </Paper>
     </Box>
+      <img src={section3img} className=" rounded-xl mr-10 w-3/12 h-2/5 mt-16" alt="section3img" />
+
+      </div>
   );
 }
