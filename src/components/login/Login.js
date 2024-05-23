dd7b0ed1b8464576727275ce2605fbff1c15229d
atishay jkain <atishay.jain@ssipmt.com>
2024-05-23 09:25:11 +0530

    auth complete

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,59 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
 import { useCookies } from 'react-cookie';
 import { Navigate } from 'react-router-dom';
 
 function Login() {
-  const [token,set_token ]= useState("")
   const [cookies, setCookie] = useCookies(['tokens']);
-  const [isAuthenticated,setIsAuthenticated]=useState(false);
-    const googlelogin =async(credentialResponse)=>{
-      // console.log(credentialResponse)
-        try{
-            const response = await fetch("http://localhost:5000/login", {
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                method: "POST",
-                body: JSON.stringify({
-                  token: credentialResponse.credential,
-                }),
-              });
-              const t=await response.json();
-              // console.log(t.authToken)
-              setCookie('tokens',t.authToken, { path: '/' })
-              if (t.authToken) {
-                setIsAuthenticated(true); 
-              }
-            } catch (error) {
-              console.log(error);
-            }
-          
-       
-          if (isAuthenticated) {
-            return <Navigate to="/" />;
-          }
-              
-        }
-      
-    
-  return (
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(() => {
+    
+    if (cookies.tokens) {
+      setIsAuthenticated(true);
+    }
+  }, [cookies]);
+
+  const googleLogin = async (credentialResponse) => {
+    try {
+      const response = await fetch("http://localhost:5000/login", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          token: credentialResponse.credential,
+        }),
+      });
+      const t = await response.json();
+      setCookie('tokens', t.authToken, { path: '/' });
+      if (t.authToken) {
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+ 
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return (
     <GoogleLogin
-    onSuccess = {credentialResponse => {
-        googlelogin(credentialResponse)
-        // if (credentialResponse.credential != null) {
-        //  const USER_CREDENTIAL = jwtDecode(credentialResponse.credential);
-        //  console.log(USER_CREDENTIAL);
-        // }
-        
-       }}
+      onSuccess={credentialResponse => {
+        googleLogin(credentialResponse);
+      }}
       onError={() => {
         console.log('Login Failed');
       }}
     />
-  )
+  );
 }
 
-export default Login
+export default Login;
