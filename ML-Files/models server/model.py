95b3ac8dcdce39a33ac23544667351ff3c82501f
atishay jkain <atishay.jain@ssipmt.com>
2025-05-18 20:39:54 +0530

    model changes

--- ML-Files/models server/model.py
+++ ML-Files/models server/model.py
@@ -9,28 +9,33 @@ model_pathDT = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\mode
 # model_pathMLP = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\classifierMLP.joblib'
 model_pathSVM = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\classifierSVM.joblib'
 model_pathNB = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\classifierNB.joblib'
-model_dia_pathNB = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\diabetes\NaiveBayes_diab_.joblib'
-model_dia_pathSVM = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\diabetes\SVM_diab_.joblib'
-model_dia_pathLR = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\diabetes\LogisticRegression_diab_.joblib'
-model_heart_grad_path= r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\heart\GradientBoostingClassifier_heart.joblib'
-model_heart_logi_path= r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\heart\LogisticRegression_heart.joblib'
-model_heart_NB_path= r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\heart\NaiveBayes_2_heart.joblib'
+model_pathXGBoost = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\XGBoost.joblib'
+model_pathQuadraticDiscriminantAnalysis = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\models server\QuadraticDiscriminantAnalysis.joblib'
+
+# model_dia_pathNB = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\diabetes\NaiveBayes_diab_.joblib'
+# model_dia_pathSVM = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\diabetes\SVM_diab_.joblib'
+# model_dia_pathLR = r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\diabetes\LogisticRegression_diab_.joblib'
+# model_heart_grad_path= r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\heart\GradientBoostingClassifier_heart.joblib'
+# model_heart_logi_path= r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\heart\LogisticRegression_heart.joblib'
+# model_heart_NB_path= r'E:\Atishay\homework\Project minor\Project-ayurved\ML-Files\heart\NaiveBayes_2_heart.joblib'
 modelRF = load(model_pathRF)
 modelK = load(model_pathk)
 modelDT = load(model_pathDT)
 modelSVM = load(model_pathSVM)
-model_dia_LR=load(model_dia_pathLR)
-model_dia_SVM=load(model_dia_pathSVM)
-model_dia_NB=load(model_dia_pathNB)
-model_heart_grad=load(model_heart_grad_path)
-model_heart_logi=load(model_heart_logi_path)
-model_heart_NB=load(model_heart_NB_path)
+modelXGB = load(model_pathXGBoost)
+modelQDA = load(model_pathQuadraticDiscriminantAnalysis)
+# model_dia_LR=load(model_dia_pathLR)
+# model_dia_SVM=load(model_dia_pathSVM)
+# model_dia_NB=load(model_dia_pathNB)
+# model_heart_grad=load(model_heart_grad_path)
+# model_heart_logi=load(model_heart_logi_path)
+# model_heart_NB=load(model_heart_NB_path)
 # modelSVM = load(model_pathSVM)
 modelNB = load(model_pathNB)
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    print(data)
+    # print(data)
     input_data = data['data'] 
    
     predictionRF = modelRF.predict(input_data).tolist()
@@ -39,58 +44,60 @@ def predict():
     predictionDT = modelDT.predict(input_data).tolist()
     predictionSVM = modelSVM.predict(input_data).tolist()
     predictionNB = modelNB.predict(input_data).tolist()
-    l1 = {"RF": predictionRF, "K": predictionK, "DT": predictionDT, "predictionSVM": predictionSVM,"predictionNB": predictionNB}
+    predictionXGB = modelXGB.predict(input_data).tolist()
+    predictionQDA = modelQDA.predict(input_data).tolist()
+    l1 = {"RF": predictionRF, "K": predictionK, "DT": predictionDT, "predictionSVM": predictionSVM,"predictionNB": predictionNB,"predictionXGB": predictionXGB,"predictionQDA": predictionQDA}
    # print(l1)
     return jsonify(l1)
-@app.route('/predictdiab', methods=['POST'])
-def predictdiab():
+# @app.route('/predictdiab', methods=['POST'])
+# def predictdiab():
     
-    data = request.get_json(force=True)
-    print(data)
+#     data = request.get_json(force=True)
+#     print(data)
 
    
-    input_data = data['data']  
+#     input_data = data['data']  
    
-    input_data_as_numpy_array = np.asarray(input_data)
+#     input_data_as_numpy_array = np.asarray(input_data)
 
     
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+#     input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
   
-    predictionLR = model_dia_LR.predict(input_data_reshaped).tolist()
-    predictionNB = model_dia_NB.predict(input_data_reshaped).tolist()
-    predictionSVM = model_dia_SVM.predict(input_data_reshaped).tolist()
+#     predictionLR = model_dia_LR.predict(input_data_reshaped).tolist()
+#     predictionNB = model_dia_NB.predict(input_data_reshaped).tolist()
+#     predictionSVM = model_dia_SVM.predict(input_data_reshaped).tolist()
 
    
-    l1 = {"LR": predictionLR, "NB": predictionNB, "SVM": predictionSVM}
+#     l1 = {"LR": predictionLR, "NB": predictionNB, "SVM": predictionSVM}
 
     
-    return jsonify(l1)
+#     return jsonify(l1)
 
 
-@app.route('/predictheart', methods=['POST'])
-def predictheart():
+# @app.route('/predictheart', methods=['POST'])
+# def predictheart():
     
-    data = request.get_json(force=True)
-    print(data)
+#     data = request.get_json(force=True)
+#     print(data)
 
    
-    input_data = data['data']  
+#     input_data = data['data']  
    
-    input_data_as_numpy_array = np.asarray(input_data)
+#     input_data_as_numpy_array = np.asarray(input_data)
 
     
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+#     input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
   
-    predictionheart = model_heart_grad.predict(input_data_reshaped).tolist()
-    predictionlogi = model_heart_logi.predict(input_data_reshaped).tolist()
-    predictionNB = model_heart_NB.predict(input_data_reshaped).tolist()
+#     predictionheart = model_heart_grad.predict(input_data_reshaped).tolist()
+#     predictionlogi = model_heart_logi.predict(input_data_reshaped).tolist()
+#     predictionNB = model_heart_NB.predict(input_data_reshaped).tolist()
 
    
-    l1 = {"LR": predictionlogi, "NB": predictionNB, "Gradient": predictionheart}
+#     l1 = {"LR": predictionlogi, "NB": predictionNB, "Gradient": predictionheart}
 
     
-    return jsonify(l1)
+    # return jsonify(l1)
 if __name__ == '__main__':
     app.run(port=5000)
